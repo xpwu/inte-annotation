160c7a9b76daa977841d01b775b101c1156ae30b
xpwu <diodexp@foxmail.com>
2019-05-15 01:41:11 +0800

    补充新的类型及数组类型

--- AnnotationHelper/test/TokenizerTest.php
+++ AnnotationHelper/test/TokenizerTest.php
@@ -35,18 +35,57 @@ class TokenizerTest extends TestCase {
   public function testGetPropertyTypeName() {
     $tokenize = new Tokenizer(new \ReflectionClass(TestGetAliasNames::class));
     $types = $tokenize->getPropertyTypeName("a");
+    self::assertEquals(1, count($types));
     self::assertEquals(TestGetAliasNamesElseClass::class, $types[0]);
 
     $types = $tokenize->getPropertyTypeName("b");
+    self::assertEquals(1, count($types));
     self::assertEquals(TestGetAliasNamesElseClass::class, $types[0]);
 
     $types = $tokenize->getPropertyTypeName("c");
+    self::assertEquals(1, count($types));
     self::assertEquals(TestGetAliasNamesElseClass::class, $types[0]);
 
     $types = $tokenize->getPropertyTypeName("d");
+    self::assertEquals(1, count($types));
     self::assertEquals(TestGetAliasNamesElseClass::class, $types[0]);
 
     $types = $tokenize->getPropertyTypeName("e");
     self::assertEquals(0, count($types));
+
+    $types = $tokenize->getPropertyTypeName("f");
+    self::assertEquals(1, count($types));
+    self::assertEquals('int', $types[0]);
+
+    $types = $tokenize->getPropertyTypeName("g");
+    self::assertEquals(2, count($types));
+    self::assertEquals('int', $types[0]);
+    self::assertEquals('null', $types[1]);
+
+    $types = $tokenize->getPropertyTypeName("h");
+    self::assertEquals(2, count($types));
+    self::assertEquals('int[]', $types[0]);
+    self::assertEquals('float', $types[1]);
+
+    $types = $tokenize->getPropertyTypeName("i");
+    self::assertEquals(1, count($types));
+    self::assertEquals('Inte\Test\ElseClass\TestGetAliasNamesElseClass[]'
+      , $types[0]);
+
+    $types = $tokenize->getPropertyTypeName("j");
+    self::assertEquals(2, count($types));
+    self::assertEquals('Inte\Test\ElseClass\TestGetAliasNamesElseClass[]'
+      , $types[0]);
+    self::assertEquals('bool', $types[1]);
+
+    $types = $tokenize->getPropertyTypeName("k");
+    self::assertEquals(2, count($types));
+    self::assertEquals('Inte\Test\ElseClass\TestGetAliasNamesElseClass[]'
+      , $types[0]);
+    self::assertEquals('bool[]', $types[1]);
+
+    $types = $tokenize->getPropertyTypeName("l");
+    self::assertEquals(0, count($types));
+
   }
 }
